dd5d4b05f367f425d5138a3a820650d36b899b8d
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2024-05-14 16:29:38 +0200

    Placed files in src

--- src/js/util.js
+++
@@ -1,8 +0,0 @@
-export const formatDate = (date) => {
-  return new Date(date).toLocaleDateString("en-US", {
-    timeZone: "UTC",
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-};
